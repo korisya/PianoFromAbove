fb85a508c52ab4efa83e6466d9aa2e321b305bc2
Khangaroo <khang06@users.noreply.github.com>
2019-07-22 16:15:58 -0400

    faster search and replace

--- PianoFromAbove/GameState.cpp
+++ PianoFromAbove/GameState.cpp
@@ -991,18 +991,7 @@ void MainScreen::UpdateState( int iPos )
         m_pNoteState[iNote] = -1;
         MIDIChannelEvent *pSearch = pEvent->GetSister();
         // linear search and erase. No biggie given N is number of simultaneous notes being played
-        vector< int >::iterator it = m_vState.begin();
-        while ( it != m_vState.end() )
-        {
-            if ( m_vEvents[*it] == pSearch )
-                it = m_vState.erase( it );
-            else
-            {
-                if ( it != m_vState.end() && m_vEvents[*it]->GetParam1() == iNote )
-                    m_pNoteState[iNote] = *it;
-                ++it;
-            }
-        }
+        m_vState.erase(std::remove_if(m_vState.begin(), m_vState.end(), [&](int x) {return m_vEvents[x] == pSearch; }));
     }
 }
 
